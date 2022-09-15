435f73cea1dc43a10c3bd3e731bdb31689b589a8
Lintao <Lintao.Amons@outlook.com>
2022-09-15 15:46:56 +0800

    init

---
+++ src/main/java/com/example/jenkinstagdemo/JenkinsTagDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstagdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTagDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTagDemoApplication.class, args);
+    }
+
+}
